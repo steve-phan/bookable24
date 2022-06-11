031e8bc270d06f13ffba86e99eb8449309b64931
Steve Phan <phanhaingoc@gmail.com>
2022-06-12 00:08:13 +0200

    refactor main account page

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,67 +1,12 @@
 import { graphql } from "gatsby"
-import React, { useEffect, useState } from "react"
-import { useQueryParam, NumberParam, StringParam } from "use-query-params"
-import { verifyPasswordResetCode, confirmPasswordReset } from "firebase/auth"
+import React from "react"
 
-import { auth } from "src/firebase"
-
-type Tmode = "resetPassword" | "recoverEmail" | "verifyEmail"
-type ToobCode = string
-
-//bookable24-61ec2.firebaseapp.com/__/auth/action?mode=resetPassword&oobCode=a2gKgZ4JJLx4tNoBk-ZeiAVWoW9JCAHffpgT9VR1soEAAAGBVKGEHg&apiKey=AIzaSyB3ycs7ejcmtHonG_NdzEQzqf-gWJp9sHU&lang=en
-
-const ResetPassword = ({ oobCode }: { oobCode: ToobCode }) => {
-  useEffect(() => {
-    handleResetPassword()
-  }, [])
-  const handleResetPassword = async () => {
-    try {
-      const email = await verifyPasswordResetCode(
-        auth,
-        (oobCode = "a2gKgZ4JJLx4tNoBk-ZeiAVWoW9JCAHffpgT9VR1soEAAAGBVKGEHg")
-      )
-      console.log({ email })
-    } catch (error) {}
-  }
-  return (
-    <>
-      <h1>ResetPassword</h1>
-      <button onClick={() => handleResetPassword()}>click</button>
-    </>
-  )
-}
-const handleMode = (mode: Tmode, oobCode: ToobCode) => {
-  switch (mode) {
-    case "resetPassword":
-      console.log("do reset password")
-      return <ResetPassword oobCode={oobCode} />
-    case "recoverEmail":
-      console.log("do  recoverEmail")
-      return <>recoverEmail</>
-    case "verifyEmail":
-      console.log("do verifyEmail")
-      return <>verifyEmail</>
-
-    default:
-      return <>Something wrong</>
-  }
-}
+//bookable24-61ec2.firebaseapp.com/__/auth/action?mode=resetPassword&oobCode=a2gKgZ4JJLx4tNoBk-ZeiAVWoW9JCAHffpgT9VR1soEAAAGBVKGEHg&
 
 const Account = () => {
-  const [UI, SetUI] = useState()
-  const mode = useQueryParam("mode") as unknown as Tmode[]
-  const oobCode = useQueryParam("oobCode") as unknown as ToobCode
-  const apiKey = useQueryParam("apiKey")
-
-  useEffect(() => {
-    // SetUI(handleMode(mode))
-  }, [mode])
-  console.log({ mode: mode[0] })
   return (
     <div>
-      <h1>Account</h1>
-
-      {handleMode(mode[0], oobCode)}
+      <h1>Account Page</h1>
     </div>
   )
 }
